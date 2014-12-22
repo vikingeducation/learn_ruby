d4b5fe58d39ae587d0c9f3a2bfc096136297d9d1
Dan Berger <dsberger@gmail.com>
2014-12-22 15:00:17 -0800

    refactored to two_digit_handler

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -4,36 +4,33 @@ class Fixnum
   @@by_tens = [nil,nil,"twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
 
   def in_words
+    words = []
     if self < 100
-      two_digit_handler(self)
-    else
-      hundreds_digit = self / 100
-      tens_digit = (self - hundreds_digit * 100) / 10
-      ones_digit = self - tens_digit * 10
-      words = []
-      if hundreds_digit != 0
-        words << @@zero_to_nineteen[hundreds_digit]
-        words << "hundred"
-      end
-
-      if tens_digit > 1
-        words << @@by_tens[tens_digit]
-      end
-
-      if ones_digit != 0
-        words << @@zero_to_nineteen[ones_digit]
-      end
-      words.join(" ")
+      words += two_digit_handler(self)
     end
+    words.join(" ")
   end
 
   def two_digit_handler(two_digit_number)
+    words = []
     if two_digit_number < 20
-      @@zero_to_nineteen[two_digit_number]
+      words << @@zero_to_nineteen[two_digit_number]
     else
-      tens_digit = two_digit_number / 10
-      ones_digit = two_digit_number - tens_digit * 10
-      words = []
+      words << twenty_to_ninety_nine(two_digit_number)
     end
+    words
   end
+
+  def twenty_to_ninety_nine(two_digit_number)
+    tens_digit = two_digit_number / 10
+    ones_digit = two_digit_number - tens_digit * 10
+    words = []
+    words << @@by_tens[tens_digit]
+    if ones_digit > 0
+      words << @@zero_to_nineteen[ones_digit]
+    end
+    words
+  end
+
+
 end
