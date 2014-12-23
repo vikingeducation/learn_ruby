20665207739f4332cb4433ecb703e95ba8d8c893
Dan Berger <dsberger@gmail.com>
2014-12-22 16:52:05 -0800

    15, millions passing, refactored with segment_handler functionality

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -1,31 +1,40 @@
 class Fixnum
+
   @@zero_to_nineteen = [nil,"one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
 
   @@by_tens = [nil,nil,"twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
 
   def in_words
-    n = self
-    if n == 0
-      return "zero"
-    end
+    
+    number = self
     words = []
 
-    millions = n / 1_000_000
-    words += handle_three_digits(millions)
-    if millions > 0
-      words << "million"
+    if number == 0
+      return "zero"
     end
-    n = n - millions * 1_000_000
 
-    thousands = n / 1_000
-    words += handle_three_digits(thousands)
-    if thousands > 0 
-      words << "thousand"
-    end
-    n = n - thousands * 1000
+    words += segment_handler(number, "million", 1_000_000)
+    number = reducer(number, 1_000_000)
 
-    words += handle_three_digits(n)
+    words += segment_handler(number, "thousand", 1_000)
+    number = reducer(number, 1_000)
+
+    words += handle_three_digits(number)
     words.compact.join(" ")
+  end
+
+  def segment_handler(number, name, value)
+    three_digit_number = number / value
+    words = handle_three_digits(three_digit_number)
+    if three_digit_number > 0
+      words << name
+    end
+    words.compact
+  end
+
+  def reducer(number, value)
+    three_digit_number = number / value
+    number - three_digit_number * value
   end
 
   def handle_three_digits(three_digit_number)
@@ -52,6 +61,5 @@ class Fixnum
     ones_digit = two_digit_number - tens_digit * 10
     [@@by_tens[tens_digit],@@zero_to_nineteen[ones_digit]].compact
   end
-
 
 end
