e8ffe25bd5cab6725da307336684767afe17b0cd
telwell <me@trevorelwell.com>
2015-03-16 23:04:45 -0400

    Question 9 w/ fixed solution

--- 09_timer/timer.rb
+++ 09_timer/timer.rb
@@ -1,52 +1,38 @@
 class Timer
 	attr_accessor :seconds 
 
-	def initialize(seconds=0)
-		@seconds = seconds
+	def initialize
+		@seconds = 0
 	end
 
 	def time_string
-		hours = 0
-		minutes = 0
-		temp_seconds = @seconds # I really only need this if there are hours involved
-		final_seconds = 0
-		final = ""
+		temp_seconds = @seconds
 
-		# this means there are only seconds no minutes
-		if @seconds/60.0 <= 1
-			final_seconds = @seconds
-
-		# now there are seconds in the order of hours
-		elsif @seconds/60.0 > 60
-			hours = ((@seconds/60.0)/60.0).floor
-			temp_seconds -= hours * 60 * 60 # remove the hour seconds
-
-			# there are also minutes here. subtract the seconds from the hours
-			# then see if there are minutes again.
-			if(temp_seconds/60.0) > 1
-				minutes = (temp_seconds/60.0).floor
-				final_seconds = temp_seconds%60
-			else
-				final_seconds = temp_seconds
-			end
-
-		# here there are seconds in the order of minutes
-		elsif @seconds/60.0 > 1
-			minutes = (@seconds/60.0).floor
-			final_seconds = @seconds%60
+		hours = temp_seconds / 3600
+		if hours > 0
+			temp_seconds -= hours * 60 * 60
 		end
 
-		# cleaning things up. if the element is only one digit then add a 0 
-		# to the end and reverse it. Otherwise just add it to final string
-		[hours,minutes,final_seconds].each do |element|
+		minutes = temp_seconds / 60
+		if minutes > 0
+			temp_seconds -= minutes * 60
+		end
+
+		time_array = [hours,minutes,temp_seconds]
+		final_array = time_array.map do |element|
+			# I saw this in dan berger's answer 
+			# "%02d" % interval
+			# not entirely sure what that does, can you explain?
+			# https://github.com/dsberger/learn_ruby/commit/17dc10d65ff43bb58d1ae53df947d2ad12b23ede
+
 			element = element.to_s
 			if element.length < 2
 				element << "0"
-				final << element.reverse + ":"
+				element.reverse
 			else
-				final << element + ":"
+				element
 			end
 		end
-		final.chomp(":")
+		final_array.join(":")
 	end
 end
---
+++ 09_timer/timer_orig.rb
@@ -0,0 +1,55 @@
+# This is my original answer which, as you can see, is a struggle. 
+# I tried again after viewing some of the other answers.
+
+class Timer
+	attr_accessor :seconds 
+
+	def initialize(seconds=0)
+		@seconds = seconds
+	end
+
+	def time_string
+		hours = 0
+		minutes = 0
+		temp_seconds = @seconds # I really only need this if there are hours involved
+		final_seconds = 0
+		final = ""
+
+		# this means there are only seconds no minutes
+		if @seconds/60.0 <= 1
+			final_seconds = @seconds
+
+		# now there are seconds in the order of hours
+		elsif @seconds/60.0 > 60
+			hours = ((@seconds/60.0)/60.0).floor
+			temp_seconds -= hours * 60 * 60 # remove the hour seconds
+
+			# there are also minutes here. subtract the seconds from the hours
+			# then see if there are minutes again.
+			if(temp_seconds/60.0) > 1
+				minutes = (temp_seconds/60.0).floor
+				final_seconds = temp_seconds%60
+			else
+				final_seconds = temp_seconds
+			end
+
+		# here there are seconds in the order of minutes
+		elsif @seconds/60.0 > 1
+			minutes = (@seconds/60.0).floor
+			final_seconds = @seconds%60
+		end
+
+		# cleaning things up. if the element is only one digit then add a 0 
+		# to the end and reverse it. Otherwise just add it to final string
+		[hours,minutes,final_seconds].each do |element|
+			element = element.to_s
+			if element.length < 2
+				element << "0"
+				final << element.reverse + ":"
+			else
+				final << element + ":"
+			end
+		end
+		final.chomp(":")
+	end
+end
