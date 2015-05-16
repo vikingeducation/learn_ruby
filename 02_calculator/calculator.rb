426d62c7ac2b85f661c8b4f8708e1d5c5ba3d994
Lonnie <wibberding@gmail.com>
2015-05-15 19:36:09 -0700

    02 Complete

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,40 @@
+def add(a,b)
+  a + b
+end
+
+def subtract (a,b)
+  a - b
+end
+
+def sum(array)
+  sum = 0 
+  array.each do |item|
+    sum += item
+  end
+  sum
+end
+
+def multiply(array)
+  product = 1
+
+  array.each do |x|
+    product *= x
+  end
+  product
+end
+
+def power(a,b)
+  a ** b
+end
+
+def factorial(num)
+  result = 1
+  if num > 1
+    array_of_numbers = (1..num).to_a
+    array_of_numbers.each do |current_number|
+      result *= current_number
+    end
+  end
+  result
+end
+
