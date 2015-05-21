3eb980b684e76f681a1d50c0bc2ae317613ec446
Adam <adam.kinson@gmail.com>
2015-05-20 22:31:56 -0400

    finish problem 12

--- 12_rpn_calculator/rpn_calculator.rb
+++ 12_rpn_calculator/rpn_calculator.rb
@@ -7,25 +7,72 @@ class RPNCalculator
     @array << input.to_i
   end
 
+  def length_check?
+    begin
+      if @array.length <= 1
+        raise "calculator is empty"
+      end
+    end
+  end
+
   def plus
-    @array << @array.pop + @array.pop
-    @value = @array.last
+    length_check?
+    @array[-2] += @array.last
+    @array.pop
   end
 
   def minus
+    length_check?
     @array[-2] -= @array.last
     @array.pop
-    @value = @array.last
   end
 
   def divide
+    length_check?
     @array[-2] /= @array[-1].to_f
     @array.pop
-    @value = @array.last
+  end
+
+  def times
+    length_check?
+    @array[-2] *= @array[-1].to_f
+    @array.pop
   end
 
   def value
-    @value
+    @value = @array.last
+  end
+
+  def tokens(string)
+    string.split(" ").map do |substring|
+      if ["+","-","*","/"].include?(substring)
+        substring.to_sym
+      else
+        substring.to_i
+      end
+    end
+  end
+
+  def evaluate(string)
+    token_array = tokens(string)
+
+    token_array.each do |token|
+      case token
+      when :+
+        plus
+      when :-
+        minus
+      when :*
+        times
+      when :/
+        divide
+      else
+        push(token)
+      end
+    end
+
+    value
+
   end
 
 end
