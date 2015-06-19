5432cd9f767a71e5d2e271fe1f315107a4b86955
Alice Fung <alice.o.fung@gmail.com>
2015-06-18 20:01:13 -0400

    add solution #12 w/o extra credit

---
+++ 12_rpn_calculator/rpn_calculator.rb
@@ -0,0 +1,54 @@
+class RPNCalculator
+
+  attr_accessor :calculator, :result
+
+  def initialize
+    @calculator = []
+  end
+
+  def push(num)
+    @calculator << num
+  end
+
+  def plus
+    check_stack
+    @result = @calculator.pop + @calculator.pop
+    @calculator.push @result
+    @result
+  end
+
+  def value
+    @result
+  end
+
+  def minus
+    check_stack
+    @result = -1 * @calculator.pop + @calculator.pop
+    @calculator.push @result
+    @result
+  end
+
+  def divide
+    check_stack
+    second = @calculator.pop
+    @result = @calculator.pop / second.to_f
+    @calculator.push @result
+    @result
+  end
+
+  def times
+    check_stack
+    @result = @calculator.pop * @calculator.pop.to_f
+    @calculator.push @result
+    @result
+  end
+
+  def check_stack
+    raise "calculator is empty" if @calculator.length < 2
+  end
+
+  def tokens(string)
+    @calculator=string.split(" ")
+  end
+
+end
