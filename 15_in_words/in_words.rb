5c50cb622c8e2eeda752446c2305283c51341f57
Alice Fung <alice.o.fung@gmail.com>
2015-06-22 14:45:47 -0400

    passed tests

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Fixnum
 
-  @@basics = {0 => nil,
+  @@basics = {0 => 'nil',
               1 => 'one',
               2 => 'two',
               3 => 'three',
@@ -42,28 +42,56 @@ class Fixnum
     num = self
 
     words = []
-#    binding.pry
+    chunk_num = 0
+
+  #  binding.pry
     if num == 0
       words << 'zero'
-    elsif num >= 1_000_000_000_000
-      #
-    elsif num >= 1_000_000_000
-      #
-    elsif num >= 1_000_000
-      #words
-    elsif num >= 1_000
-      words += [hundreds_or_less(num / 1000), "thousand"]
-      if num % 1000 != 0
-        words << hundreds_or_less(num % 1000)
-      end
     elsif num < 1_000
       words << hundreds_or_less(num)
+
+    elsif num >= 1_000
+#      words += [hundreds_or_less(num / 1000), "thousand"]
+#      if num % 1000 != 0
+#        words << hundreds_or_less(num % 1000)
+#      end
+
+      three_digit_chunks = increments(num)
+      chunk_num = (three_digit_chunks.length) -1
+      three_digit_chunks.each do |chunk| #array of three digit translations
+        base = 1000
+        if chunk != 0
+          words << hundreds_or_less(chunk)
+          base **= chunk_num
+          words << @@num_increments[base]
+        end
+
+        chunk_num -= 1
+      end
+
     else
       "cannot process"
     end
+    words -= [nil] if num > 0
     words.join (" ")
   end
 
+
+  def increments(num) #split number into 3digit chunks
+   # binding.pry
+    three_digits=[]
+    #num >= 1000 ? chunk_num = 1 : chunk_num = 0 #if this is initiated, already thousand
+    #chunk_num = 0
+    while num >= 1000   #checks if more than 3 digits long
+      three_digits << num % 1000  #adds last 3 digits of number
+      num /= 1000
+    #  chunk_num += 1 #counts increments: thousands, millions ...
+    end
+
+    three_digits << num
+
+    three_digits.reverse #<< chunk_num
+  end
 
   def hundreds_or_less(num)
     if num < 10
@@ -94,4 +122,4 @@ class Fixnum
 
 end
 
-puts 100.in_words
+puts 10_000_001.in_words
