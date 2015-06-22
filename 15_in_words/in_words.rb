d3c00c847258961fa127dc2e2502fa4bd031fd74
Alice Fung <alice.o.fung@gmail.com>
2015-06-21 21:34:15 -0400

    re-did #<100

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -2,111 +2,68 @@ require 'pry'
 
 class Fixnum
 
+
+
   def in_words
     num = self
-    case num
-    when 0..19
-      num.num_less_than_20
-    when 20..99
-      num.num_less_than_100
-    when 100..999
-      num.bigger_num
-    end
-  end
+    basics = {0 => 'zero',
+            1 => 'one',
+            2 => 'two',
+            3 => 'three',
+            4 => 'four',
+            5 => 'five',
+            6 => 'six',
+            7 => 'seven',
+            8 => 'eight',
+            9 => 'nine',
+            10 => 'ten',
+            11 => 'eleven',
+            12 => 'twelve',
+            13 => 'thirteen',
+            14 => 'fourteen',
+            15 => 'fifteen',
+            16 => 'sixteen',
+            17 => 'seventeen',
+            18 => 'eighteen',
+            19 => 'nineteen',
+            20 => 'twenty',
+            30 => 'thirty',
+            40 => 'forty',
+            50 => 'fifty',
+            60 => 'sixty',
+            70 => 'seventy',
+            80 => 'eighty',
+            90 => 'ninety',
+            100 => 'hundred',
+            1_000 => 'thousand',
+            1_000_000 => 'million',
+            1_000_000_000 => 'billion',
+            1_000_000_000_000 => 'trillion'}
 
-
-  def num_less_than_20
-    case self
-    when 0
-      'zero'
-    when 1
-      'one'
-    when 2
-      'two'
-    when 3
-      'three'
-    when 4
-      'four'
-    when 5
-      'five'
-    when 6
-      'six'
-    when 7
-      'seven'
-    when 8
-      'eight'
-    when 9
-      'nine'
-    when 10
-      'ten'
-    when 11
-      'eleven'
-    when 12
-      'twelve'
-    when 13
-      'thirteen'
-    when 14
-      'fourteen'
-    when 15
-      'fifteen'
-    when 16
-      'sixteen'
-    when 17
-      'seventeen'
-    when 18
-      'eighteen'
-    when 19
-      'nineteen'
-    end
-  end
-
-  def tens
-    case self
-    when 20
-      'twenty'
-    when 30
-      'thirty'
-    when 40
-      'forty'
-    when 50
-      'fifty'
-    when 60
-      'sixty'
-    when 70
-      'seventy'
-    when 80
-      'eighty'
-    when 90
-      'ninety'
-    end
-  end
-
-  def num_less_than_100
-    num = self
-    if num % 10 == 0
-      num.tens
-    else
-     (num / 10 * 10).tens + " #{(num % 10).num_less_than_20}"
-   end
-  end
-
-  def bigger_num
+    words = []
 #    binding.pry
-    words=[]
-    num = self
-    case num
-    when num < 1_000
-      words << (num / 100).num_less_than_100 + " hundred"
-      if num % 100 != 0
-#        puts num % 100
-        words << " #{(num%100).num_less_than_100}"
-      end
-      words.join (" ")
-    when num < 1_000_000
-      puts "what am I doing... there's an easier way"
+    if basics.keys.include? num
+      words << basics[num]
+    elsif num > 1_000_000_000_000
+      #
+    elsif num > 1_000_000_000
+      #
+    elsif num > 1_000_000
+      #
+    elsif num > 1_000
+      #
+    elsif num > 100
+      #
+    elsif num < 100
+      words << basics[num / 10 * 10]
+      words << basics [num % 10]
+    else
+      "cannot process"
     end
-    words
+    words.join (" ")
   end
+
+
 
 end
 
