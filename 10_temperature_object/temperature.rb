baaae7ddc347655f852314e58af723f7f3ccbbfc
Joshua Masland-Sarani <j.masland@gmail.com>
2015-10-31 17:15:14 -0400

    implement alternate constructor methods and subclasses

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -11,6 +11,14 @@ class Temperature
   def in_celsius
     return @celsius
   end
+               
+  def self.from_celsius(degrees)
+    new(:c => degrees)
+  end
+
+  def self.from_fahrenheit(degrees)
+    new(:f => degrees)
+  end
 
   def ftoc(temp_f)
     ( 5.0 / 9.0 ) * ( temp_f - 32 )
@@ -18,5 +26,19 @@ class Temperature
 
   def ctof(temp_c)
     ( 9.0 / 5.0 ) * temp_c + 32
+  end
+end
+
+class Celsius < Temperature
+  def initialize(temperature)
+    @celsius = temperature
+    @fahrenheit = ctof(temperature)
+  end
+end
+
+class Fahrenheit < Temperature
+  def initialize(temperature)
+    @celsius = ftoc(temperature)
+    @fahrenheit = temperature
   end
 end
