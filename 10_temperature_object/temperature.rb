19a63c0dd60e935cf555d24d4071c886452fa4b3
Joshua Masland-Sarani <j.masland@gmail.com>
2015-10-31 16:50:23 -0400

    implement options hash

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -1,2 +1,22 @@
 class Temperature
+  def initialize(temperature)
+    @fahrenheit = temperature[:f] || ctof(temperature[:c])
+    @celsius = temperature[:c] || ftoc(temperature[:f])
+  end
+
+  def in_fahrenheit 
+    return @fahrenheit
+  end
+
+  def in_celsius
+    return @celsius
+  end
+
+  def ftoc(temp_f)
+    ( 5.0 / 9.0 ) * ( temp_f - 32 )
+  end
+
+  def ctof(temp_c)
+    ( 9.0 / 5.0 ) * temp_c + 32
+  end
 end
