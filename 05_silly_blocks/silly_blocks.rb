6ecb1741f0bda24b2f2890bad982104e4d951992
Joshua Masland-Sarani <j.masland@gmail.com>
2015-10-31 15:00:36 -0400

    edit silly_blocks

--- 05_silly_blocks/silly_blocks.rb
+++ 05_silly_blocks/silly_blocks.rb
@@ -1,11 +1,16 @@
 def reverser
   word_array = yield.split(" ")
-  word_array.each { |word| "#{word.reverse!}" }
+  word_array.each do |word| 
+    word.reverse!
+    word << " "
+  end
+
   return word_array.join.strip
 end
 
-def adder
-
+def adder(amount_to_add=1)
+  new_number = yield + amount_to_add
+  return new_number
 end
 
 def repeater
