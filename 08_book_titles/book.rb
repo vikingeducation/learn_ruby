e1f7dc4eb609ef3150150f492de1e913ac6232d2
Joshua Masland-Sarani <j.masland@gmail.com>
2015-10-31 16:02:17 -0400

    edit book.rb to pass remaining tests

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -1,13 +1,18 @@
 class Book
-  articles = ["a", "an", "the"]
+  
 
   @title = "title"
   def title=(title)
+    no_caps = ["a", "an", "the", "and", "in", "of"]
+    always_caps = ["I"]
     word_array = title.split(" ")
     word_array.each do |word| 
-      word.capitalize!
+      if !no_caps.include?(word)
+        word.capitalize!
+      end
       word << " "
     end
+    word_array[0].capitalize!
     @title = word_array.join.strip
   end
 
