1015ba7aa0fc3666389c06942ad6e7b9e07ae8e1
Joshua Masland-Sarani <j.masland@gmail.com>
2015-11-01 12:58:03 -0500

    edit dictionary.rb

--- 11_dictionary/dictionary.rb
+++ 11_dictionary/dictionary.rb
@@ -1,9 +1,25 @@
 class Dictionary
   def initialize 
     @entries = {}
+    @keywords = []
   end
 
   def entries
     @entries
+  end
+
+  def keywords
+    @keywords
+  end
+
+  def add(entry)
+    if entry.class == Hash
+      @entries.merge!(entry)
+      @keywords << entry.keys[0]
+    elsif entry.class == String
+      entry_hash = {entry => nil}
+      @entries.merge!(entry_hash)
+      @keywords << entry_hash.keys[0]
+    end
   end
 end
