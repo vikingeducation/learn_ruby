9ba6143f19f0fa400415e3e9ec90ba2ede498ba8
Sia <siakaramalegos@gmail.com>
2015-11-02 13:48:28 -0600

    Finishes array extensions

---
+++ 14_array_extensions/array_extensions.rb
@@ -0,0 +1,19 @@
+class Array
+
+  def sum
+    result = 0
+    self.each { |number| result += number }
+    result
+  end
+
+  def square
+    results = []
+    self.each { |number| results << number ** 2 }
+    results
+  end
+
+  def square!
+    self.map! { |number| number ** 2 }
+  end
+
+end
