b3f2a32331026b27b0c1e181b0416ecbc58db014
Thomas Lo <wagaiznogoud@gmail.com>
2015-11-14 17:50:26 -0800

    Solution to dictionary

---
+++ 11_dictionary/dictionary.rb
@@ -0,0 +1,38 @@
+class Dictionary
+
+	def initialize
+		@entries = {}
+		@keywords = []
+	end
+
+	def entries
+		@entries
+	end
+
+	def add word_entry
+		if word_entry.class == Hash
+			word_entry.each {|key, value| @entries[key] = value}
+		else
+			@entries[word_entry] = nil
+		end
+		@keywords = @entries.keys 
+	end
+
+	def keywords
+		@keywords.sort!
+	end
+
+	def include? word
+		@keywords.include?(word)
+	end
+
+	def find word
+		@entries.select {|key, val| key.include?(word)}
+	end
+
+	def printable
+		result = ""
+		@keywords.sort.each {|key| result << "[#{key}] \"#{@entries[key]}\"\n"}
+		result.chomp
+	end
+end
