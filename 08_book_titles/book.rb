cf57bf2e3c1ec5d4360e408eb21051f71d28b783
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-02-11 16:33:42 -0500

    Wrote book.rb, passed all 9 tests

---
+++ 08_book_titles/book.rb
@@ -0,0 +1,41 @@
+class Book
+
+#instantiates variable values
+    def initialize
+        @title = ""
+    end
+
+#allows for setting a new value (assigns value to class(instance) variable)
+    def title=(string)
+        @title = string
+    end
+
+# gets the variable and changes it (not typical "getter" use)
+# --Allows you to read the variable and change it
+    def title
+
+# I reused all this code from the 03_simon_says code, so if you passed those tests,
+# you can reuse the formula for these, adding cases for specific words,
+# like prepositions/conjunctions and so forth.
+
+
+        @title_words = @title.split
+        @conjunctions = ["for", "and", "nor", "but", "or", "yet", "so"]
+        @prepositions = ["in", "the", "of"]
+        @articles = ["a", "an"]
+        @proper_case = []
+        @title_words.each_with_index do |word, index|
+            word = word.capitalize
+            if((@conjunctions.include?(word.downcase) || @prepositions.include?(word.downcase) || @articles.include?(word.downcase)) && index != 0)
+                word = word.downcase
+            end
+            @proper_case.push(word)
+        end
+        return @proper_case.join(" ")
+
+    end
+
+
+
+end
+
