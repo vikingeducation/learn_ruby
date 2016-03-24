ff0d8a72ecd0e4fe5a3990f2bc1531279470bab4
Ali SARIALTUN <alisarialtun@outlook.com>
2016-03-24 13:40:25 +0000

    pig latin completed

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -4,7 +4,11 @@ def translate (word)
     #word has become an array of words to be translated into pig latin
     word.map! do |a|
         while ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"].include?(a[0])
-          a=  a[1..-1]+a[0] 
+          if a[0..1] == "qu"
+              a = a[2..-1]+"qu"
+          else
+          a=  a[1..-1]+a[0]
+          end
         end
         a = a +"ay"
     end
