f847e834bcba3bf26cdcb18c9bae897bc6adf2e4
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-05-17 09:55:53 -0700

    Create Temperature class to pass first set of tests in 10_temperature_object

---
+++ 10_temperature_object/temperature.rb
@@ -0,0 +1,32 @@
+
+class Temperature
+
+  def initialize(options = {})
+    options.each do |scale, temperature|
+      if scale == :c
+        @celsius = temperature
+        @fahrenheit = (temperature * (9/5.0)) + 32
+      elsif scale  == :f
+        @fahrenheit = temperature
+        @celsius =  5/9.0 * (temperature - 32)
+      end
+    end
+  end
+
+  def in_celsius
+    return @celsius
+  end
+
+  def in_fahrenheit
+    return @fahrenheit
+  end
+
+  def self.from_celsius(temperature)
+    Temperature.new(c:temperature)
+  end
+
+  def self.from_fahrenheit(temperature)
+    Temperature.new(f:temperature)
+  end
+
+end
