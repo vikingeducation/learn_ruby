5185040e4694e9353ac0f936b1f0030af0733c56
Joe Bernardi <joebernardi@hotmail.com>
2016-05-19 22:43:12 -0500

    start of first refactor

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -4,6 +4,15 @@
 def translate(s)
 
 
+
+	def one_vowel( word )
+
+				word.start_with?( "a", "e", "i", "o", "u" ) ? true : false
+
+	end
+
+
+
 	# create a variable that lets us determine if there are more than one word by using an array we can pass 'count' on
 	words = s.split(" ")
 
@@ -15,7 +24,7 @@ def translate(s)
 
 
 			# check for aeiou at the start and append 'ay'
-			if s.start_with?( "a", "e", "i", "o", "u" )
+			if one_vowel(s) == true
 
 				# translating any vowel
 				s + "ay"
@@ -61,29 +70,80 @@ def translate(s)
 
 			end
 
+
+
+
+
+
+
+
+
+
+		# called if there are two words or more
 		else
 
-			"WHAAAAAT!?"
+
+
+			words.map { |w|
+
+				if one_vowel(w) == true
+
+					# translating any vowel
+					w + "ay"
+
+
+
+
+				# check if the second letter is a vowel then translate
+				elsif w.chars[1].start_with?( "a", "e", "i", "o", "u" )
+
+					# store the letters in an array
+					ch_array = w.chars
+
+					# then grab the first letter from the array
+					first_letter = ch_array.shift
+
+					# put that letter at the back with push and add 'ay'
+					ch_array << first_letter + "ay"
+
+					# bring it back together
+					ch_array.join
+
+
+
+
+				# runs if there are two consonants in the array
+				elsif
+
+					# store the letters in an array
+					ch_array = w.chars
+
+					# then grab the first letter from the array
+					first_letter = ch_array.shift
+
+					# then grab the second letter from the array
+					second_letter = ch_array.shift
+
+					# put both letters at the back with push and add 'ay'
+					ch_array << first_letter + second_letter + "ay"
+
+					# bring it back together
+					ch_array.join
+
+				end}.join(" ")
+
+
 
 		end # /.if count_of_words == 1
 
-end
 
 
 
-def one_vowel
 
 end
 
 
 
-def one_consanant
 
-end
-
-
-def two_consonants
-
-end
 
 
