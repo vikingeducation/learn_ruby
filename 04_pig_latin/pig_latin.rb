e6a2cb740e6ad9a1ceedfbd9b6c58c58905f0ad0
Joe Bernardi <joebernardi@hotmail.com>
2016-05-19 22:24:27 -0500

    add two consonants test

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,48 +1,89 @@
+# /pig_latin.rb
 # traslate a word beginning with a vowel - all one method
 
 def translate(s)
-	# we need to place 'ay' at the end of the word
-	# if it starts with a vowel, the word is unchanged and 'ay' appended
 
 
-	# check for aeiou at the start and append 'ay'
-	if s.start_with?( "a", "e", "i", "o", "u" )
-
-		# translating any vowel
-		s + "ay"
-
-	# check if the second letter is a vowel then translate
-	elsif s.chars[1].start_with?( "a", "e", "i", "o", "u" )
-
-		# store the letters in an array
-		ch_array = s.chars
-
-		# then grab the first letter from the array
-		first_letter = ch_array.shift
-
-		# put that letter at the back with push and add 'ay'
-		ch_array << first_letter + "ay"
-
-		# bring it back together
-		ch_array.join
-
-	end
-
-	# we could chars the word, store the first letter in a var and add it to the array + ay then join
+	# create a variable that lets us determine if there are more than one word by using an array we can pass 'count' on
+	words = s.split(" ")
 
 
+
+		# check how many words there are in the string
+		if words.count == 1
+
+
+
+			# check for aeiou at the start and append 'ay'
+			if s.start_with?( "a", "e", "i", "o", "u" )
+
+				# translating any vowel
+				s + "ay"
+
+
+
+
+			# check if the second letter is a vowel then translate
+			elsif s.chars[1].start_with?( "a", "e", "i", "o", "u" )
+
+				# store the letters in an array
+				ch_array = s.chars
+
+				# then grab the first letter from the array
+				first_letter = ch_array.shift
+
+				# put that letter at the back with push and add 'ay'
+				ch_array << first_letter + "ay"
+
+				# bring it back together
+				ch_array.join
+
+
+
+
+			# runs if there are two consonants in the array
+			elsif
+
+				# store the letters in an array
+				ch_array = s.chars
+
+				# then grab the first letter from the array
+				first_letter = ch_array.shift
+
+				# then grab the second letter from the array
+				second_letter = ch_array.shift
+
+				# put both letters at the back with push and add 'ay'
+				ch_array << first_letter + second_letter + "ay"
+
+				# bring it back together
+				ch_array.join
+
+			end
+
+		else
+
+			"WHAAAAAT!?"
+
+		end # /.if count_of_words == 1
 
 end
 
 
-=begin
 
-	array.each_with_index.map { | w , i |
+def one_vowel
 
-			if ( w == "and" || w == "or" || w == "the" || w == "over" ) && i != 0
-				w
-			else
-				w.capitalize
-			end }.join(" ")
+end
 
-=end
+
+
+def one_consanant
+
+end
+
+
+def two_consonants
+
+end
+
+
