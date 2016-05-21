f7c4f9558ab247c214c819733e511bf9340694a0
Joe Bernardi <joebernardi@hotmail.com>
2016-05-20 20:47:22 -0500

    translate many words to complete

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -23,7 +23,7 @@ def translate(s)
 
 
 
-	def one_consonant(s)
+	def translate_one_consonant(s)
 
 		# store the letters in an array
 		ch_array = s.chars
@@ -42,7 +42,7 @@ def translate(s)
 
 
 
-	def two_consonants(s)
+	def translate_two_consonants(s)
 
 				# store the letters in an array
 				ch_array = s.chars
@@ -59,111 +59,90 @@ def translate(s)
 				# bring it back together
 			  ch_array.join
 
+	end
+
+
+	def translate_three_consonants( s )
+
+				# store the letters in an array
+				ch_array = s.chars
+
+				# then grab the first letter from the array
+				first_letter = ch_array.shift
+
+				# then grab the second letter from the array
+				second_letter = ch_array.shift
+
+				# then grab the third letter from the array
+				third_letter = ch_array.shift
+
+				# put both letters at the back with push and add 'ay'
+				ch_array << first_letter + second_letter + third_letter + "ay"
+
+				# bring it back together
+			  ch_array.join
 
 	end
 
 
 
-	# create a variable that lets us determine if there are more than one word by using an array we can pass 'count' on
+	# store the word in an array with split so we can translate multiple words with each if multiples
 	words = s.split(" ")
 
 
-
 		# check how many words there are in the string
-		if words.count == 1
-
+		# if words.count == 1
+	words.map { | w |
 
 
 			# check for aeiou at the start and append 'ay'
-			if one_vowel?(s) == true
+			if one_vowel?( w )
 
 				# translating any vowel
-				translate_one_vowel( s )
+				translate_one_vowel( w )
 
 
 
 			# check if the second letter is a vowel then translate
-			elsif s.chars[1].start_with?( "a", "e", "i", "o", "u" )
+			elsif s.chars[1].start_with?( "a", "e", "i", "o", "u" ) && s.chars[0] != "q"
 
-				one_consonant(s)
+				translate_one_consonant( w )
 
+			elsif s.chars[0].start_with?("q") && s.chars[1].start_with?("u")
 
+				translate_two_consonants( w )
+
+			elsif  s.chars[1].start_with?("q")
+
+				translate_three_consonants( w )
 
 			# runs if there are two consonants in the array
+			elsif s.chars[2].start_with?( "a", "e", "i", "o", "u" )
+
+				translate_two_consonants( w )
+
 			else
 
-				two_consonants(s)
+				translate_three_consonants( w )
 
 			end
 
-
-
-
-
-
-
-		# called if there are two words or more
-		else
-
-
-
-			words.map { |w|
-
-				if one_vowel(w) == true
-
-					# translating any vowel
-					w + "ay"
-
-
-
-
-				# check if the second letter is a vowel then translate
-				elsif w.chars[1].start_with?( "a", "e", "i", "o", "u" )
-
-					# store the letters in an array
-					ch_array = w.chars
-
-					# then grab the first letter from the array
-					first_letter = ch_array.shift
-
-					# put that letter at the back with push and add 'ay'
-					ch_array << first_letter + "ay"
-
-					# bring it back together
-					ch_array.join
-
-
-
-
-				# runs if there are two consonants in the array
-				elsif
-
-					# store the letters in an array
-					ch_array = w.chars
-
-					# then grab the first letter from the array
-					first_letter = ch_array.shift
-
-					# then grab the second letter from the array
-					second_letter = ch_array.shift
-
-					# put both letters at the back with push and add 'ay'
-					ch_array << first_letter + second_letter + "ay"
-
-					# bring it back together
-					ch_array.join
-
-				end}.join(" ")
-
-
-
-		end # /.if count_of_words == 1
-
-
+}.join(" ")
 
 
 
 end
+
+
+		# called if there are two words or more
+
+
+
+
+
+
+
+
 
 
 
