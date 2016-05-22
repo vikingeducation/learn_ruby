42acb5ef3e4896aa8fe892d921c2d1bd71242e60
Joe Bernardi <joebernardi@hotmail.com>
2016-05-22 11:05:14 -0500

    repeater

--- 05_silly_blocks/silly_blocks.rb
+++ 05_silly_blocks/silly_blocks.rb
@@ -4,28 +4,44 @@ require 'pry'
 
 
 
-def reverser
+def reverser(&block)
 
-	result = yield
-	result.split(" ").map { |w| w.reverse }.join(" ")
-
+	block.call.split(" ").map { |w| w.reverse }.join(" ")
 
 end
 
-def adder(*num)
 
-	if num.empty?
-		num = 1
+
+
+
+
+def adder(num=1, &block)
+
+	block.call + num
+
+end
+
+
+
+
+def repeater(num=1, &block)
+
+	i = 1
+	until i > num
+	 yield if block_given?
+	 i += 1
 	end
 
-	result = yield num
 
-	result.join + num
 
 end
 
 
-reverser { }
+
+
+
+
+
 
 
 
