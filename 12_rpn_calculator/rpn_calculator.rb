65f388a233d79355951f9ef12a09125081990705
Joe Bernardi <joebernardi@hotmail.com>
2016-05-23 21:33:42 -0500

    work on value

--- 12_rpn_calculator/rpn_calculator.rb
+++ 12_rpn_calculator/rpn_calculator.rb
@@ -24,8 +24,10 @@ class RPNCalculator
 
 	def value
 
+		length_of_calc = @calculator.count
 		num_array = []
 		op_array = []
+		result_array = []
 		# we determine what operands are in the array
 		# we determine how many numbers we have to calculate
 		# if there is only one operand, we check there are only 2 nums
@@ -35,6 +37,14 @@ class RPNCalculator
 			else
 				op_array << num
 			end}
+
+		if length_of_calc == 3
+				result_array << num_array.shift
+				result_array << op_array.pop
+				result_array << num_array.shift
+		end
+
+		result_array.map { |x| }
 
 	end
 
