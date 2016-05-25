80e461e0f325f879efad6e55443f1450f218518c
RobertBMerriman <robertbmerriman@gmail.com>
2016-05-25 16:37:56 +0100

    updating calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,11 +1,24 @@
 def add x, y
-
+    x + y
 end
 
 def subtract x, y
-
+    x - y
 end
 
 def sum numbers
+    sum = 0
+    numbers.each do |num|
+        sum += num
+    end
 
+    sum
+end
+
+def multiply x, y
+    x * y
+end
+
+def power x, y
+    x ** y
 end
