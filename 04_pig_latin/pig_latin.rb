8cfcb453dbc332b062e6ed705c8dd57a227abca2
RobertBMerriman <robertbmerriman@gmail.com>
2016-05-26 10:41:09 +0100

    Almost finished pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,17 +1,36 @@
 def translate text
     words = text.split
 
+    # Iterate over every word and change it in place
     words.map! do |word|
 
-        word.each_char.with_index do |char, index|
+        vowel_pos = find_vowel_pos word # Get first vowel in the word
 
+        # If vowel_pos is nil, there are no vowels so return the word unchanged + "ay"
+        if vowel_pos.nil?
+            return word + "ay"
         end
+
+        prefix = word[0, vowel_pos]
+        suffix = word[vowel_pos, word.length]
+
+        word = suffix + prefix + "ay"
     end
 
     words.join(" ")
 end
 
 private
+    def find_vowel_pos word
+        # Iterate over each character in the word with an index
+        word.each_char.with_index do |char, index|
+            # If the character is a vowel, save the index and break
+            if vowel?(char)
+                return index
+            end
+        end
+    end
+
     def vowel? char
         ["a", "e", "i", "o", "u"].include? char
     end
