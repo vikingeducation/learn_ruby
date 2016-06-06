a814c74e02bdbc432f0082a7b14e3ba4c89bb77a
jsteenb2 <jonathansteenbergen@gmail.com>
2016-06-05 19:18:13 -0700

    finished 03 challenge

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -11,6 +11,16 @@ def sum(arr)
 	arr.inject(0){|sum, ele| sum + ele}
 end
 
-def multiply(a, b)
+def multiply(a, b, *nums)
+	return nums.inject(a*b) {|product, ele| product * ele} unless nums.empty?
 	a * b
+end
+
+def power(a, b)
+	a ** b
+end
+
+def factorial(a)
+	return 1 if a == 0
+	a * factorial(a - 1)
 end
