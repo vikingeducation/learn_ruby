a32c065d7130d99e926103141af9d09cfdbbc469
essian <jessicampdennis@hotmail.com>
2016-06-23 18:00:51 +0100

    adds solution 13

---
+++ 13_xml_document/xml_document.rb
@@ -0,0 +1,33 @@
+# copied this from http://stackoverflow.com/questions/33108896/test-first-ruby-master-xml-builder
+
+class XmlDocument
+      def initialize(indent = false)
+        @indent = indent
+        @indent_level = 0
+      end
+
+      def method_missing(method_name, *args, &block)
+        attributes = args[0] || {}
+        s = ""
+        s << ("  " * @indent_level) if @indent
+        s << "<#{method_name}"
+        attributes.each_pair do |key, value|
+          s << " #{key}='#{value}'"
+        end
+        if block
+          s << ">"
+          s << "\n" if @indent
+          @indent_level += 1
+          s << yield
+          @indent_level -= 1
+          s << ("  " * @indent_level) if @indent
+          s << "</#{method_name}>"
+          s << "\n" if @indent
+
+        else
+          s << "/>"
+          s << "\n" if @indent
+        end
+        s
+      end
+    end
