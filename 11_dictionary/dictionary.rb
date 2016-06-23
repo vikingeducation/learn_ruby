027d232953f42b533a8f465d27d9d94861b19431
essian <jessicampdennis@hotmail.com>
2016-06-23 15:11:30 +0100

    adds solution 11

---
+++ 11_dictionary/dictionary.rb
@@ -0,0 +1,43 @@
+class Dictionary
+
+  attr_accessor :entries
+
+  def initialize
+    @entries = {}
+  end
+
+  def add(entry)
+    entry = {entry => nil} if entry.is_a?(String)
+    @entries.merge!(entry)
+  end
+
+  def keywords
+    @entries.keys.sort
+  end
+
+  def include?(key)
+    @entries.has_key?(key)
+  end
+
+  def find(str)
+    result = {}
+    @entries.each {|key, value| result[key]=value if key.start_with?(str) }
+    result
+  end
+
+  def printable
+    string = ''
+    @entries.sort.each do |key, value|
+      string = string + %Q{[#{key}] "#{value}"\n}
+    end
+    string.chomp
+  end
+
+  
+
+
+end
+
+d = Dictionary.new
+
+p d.find('fish')
