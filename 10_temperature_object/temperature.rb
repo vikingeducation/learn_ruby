683ffb6418819c532f3096c9a0ca2f2c3840b050
essian <jessicampdennis@hotmail.com>
2016-06-23 13:47:48 +0100

    adds solution 10

---
+++ 10_temperature_object/temperature.rb
@@ -0,0 +1,48 @@
+class Temperature
+
+  attr_accessor :celsius
+
+  def initialize(opt={})
+    @farenheit = opt[:f]
+    @celsius = opt[:c]
+  end
+
+  def in_fahrenheit
+    return @farenheit if @farenheit
+
+    ((@celsius * (1.8))+32) || :f
+
+  end
+
+  def in_celsius
+    return @celsius if @celsius
+    ((@farenheit - 32) / 1.8).round || :c
+  end
+
+  def self.from_celsius(temp)
+    Temperature.new(c:temp)
+  end
+
+  def self.from_fahrenheit(temp)
+    Temperature.new(f:temp)
+  end
+
+  end
+
+  class Celsius < Temperature
+
+    def initialize(temp)
+      @celsius = temp
+    end
+
+  end
+
+  class Fahrenheit < Temperature
+
+    def initialize(temp)
+      @farenheit = temp
+    end
+
+  end
+
+ p Temperature.from_celsius(50).in_celsius
