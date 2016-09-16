3b5d3d64ef20ed0183af73e5c3ff62c3e8d9fcc7
Jeffrey <jeffrey.d.sun@gmail.com>
2016-09-16 01:39:12 -0700

    silly blocks

--- 05_silly_blocks/silly_blocks.rb
+++ 05_silly_blocks/silly_blocks.rb
@@ -1,3 +1,11 @@
-def reverser str
-	str.reverse!
+def reverser(&block)
+  yield.split(" ").map(&:reverse).join(" ")
+end
+
+def adder(num=1, &block)
+  block.call + num
+end
+
+def repeater(n=1, &block)
+  n.times &block
 end
