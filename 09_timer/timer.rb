5f9e28c9557705e475b66d3797c5deeedf8e47a5
Jeffrey <jeffrey.d.sun@gmail.com>
2016-09-26 23:12:03 -0700

    redo hw timer

---
+++ 09_timer/timer.rb
@@ -0,0 +1,26 @@
+class Timer
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    [hours, minutes, m_seconds].map(&method(:padded)).join(":")
+  end
+
+  def hours
+    seconds / 3600
+  end
+
+  def minutes
+    (seconds % 3600)/60
+  end
+
+  def m_seconds
+    (seconds % 60)
+  end
+
+  def padded(n)
+    "#{n}".rjust(2, '0')
+  end
+end
