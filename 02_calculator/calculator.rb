8fbd5ba1f69b0960df937fac6abf333ef58a44c1
Jake Tingley <jaket32@gmail.com>
2016-10-21 13:37:26 -0400

    finished calculator.rb

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,15 @@
+def add(num1, num2)
+  sum = num1 + num2
+  return sum
+end
+
+def subtract(num1, num2)
+  difference = num1 - num2
+  return difference
+end
+
+def sum(arr)
+  total = 0
+  arr.each{|x| total += x }
+  return total
+end
