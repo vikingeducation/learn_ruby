ca0786de9d7b5097a092cd8a198c0f1de9676fe1
Sampson Crowley <sampsonsprojects@gmail.com>
2016-10-27 17:58:37 +0000

    09_timer add manual method for string conversion

--- 09_timer/timer.rb
+++ 09_timer/timer.rb
@@ -5,6 +5,13 @@ class Timer
   def seconds=(seconds)
     @seconds = seconds
     @time_string = Time.at(seconds).utc.strftime("%H:%M:%S")
+    
+    #Manual Method
+
+    #hours = seconds/3600
+    #minutes = seconds % 3600 / 60
+    #seconds = seconds % 3600 % 60
+    #@time_string = "#{(hours<10 ? '0'+hours.to_s : hours)}:#{(minutes<10 ? '0'+minutes.to_s : minutes)}:#{(seconds<10 ? '0'+seconds.to_s : seconds)}"
   end
   def seconds
     @seconds
