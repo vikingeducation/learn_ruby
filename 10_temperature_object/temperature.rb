7a4754823145e5dd860fbaaec96ad3cdba059772
James Harris <computersherpa@gmail.com>
2016-10-27 21:53:27 -0500

    Factory method

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -9,4 +9,10 @@ class Temperature
   def in_celsius
     @c || (@f.to_f - 32.0) * 5.0/9.0
   end
+  def self.from_celsius(c)
+    Temperature.new({:c => c})
+  end
+  def self.from_fahrenheit(f)
+    Temperature.new({:f => f})
+  end
 end
