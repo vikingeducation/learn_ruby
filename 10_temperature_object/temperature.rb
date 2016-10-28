2b37c604f99a894ebb69206d61da62b0be3c1793
James Harris <computersherpa@gmail.com>
2016-10-27 21:38:47 -0500

    More temperature.rb WIP

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -1,11 +1,12 @@
 class Temperature
   def initialize(options={})
-
+    @f = options[:f] ||= options[:fahrenheit]
+    @c = options[:c] ||= options[:celsius]
   end
-  def in_fahrenheit(c)
-    c.to_f * 9.0/5.0 + 32
+  def in_fahrenheit
+    @f || @c.to_f * 9.0/5.0 + 32
   end
-  def in_celsius(f)
-    (f.to_f - 32.0) * 5.0/9.0
+  def in_celsius
+    @c || (@f.to_f - 32.0) * 5.0/9.0
   end
 end
