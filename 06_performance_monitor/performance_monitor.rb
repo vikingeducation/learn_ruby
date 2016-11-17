44b3d90dd782ac4cb55c0b99263deaedb4b4570e
andie5 <andieasa@googlemail.com>
2016-11-17 00:28:20 +0000

    performance monitor some tests

--- 06_performance_monitor/performance_monitor.rb
+++ 06_performance_monitor/performance_monitor.rb
@@ -1,6 +1,21 @@
 # TDD RSpec
 # performance_monitor.rb
 
-def measure
- 
+def measure(n=1)
+  time_start = Time.now
+  if (n>1)
+    n.times do
+      yield
+    end
+  else
+    yield
+  end
+
+  (Time.now - time_start) / (n=1 ? 1 : n)
+end
+
+measure do
+end
+
+measure(4) do
 end
