83ae1de5214fe1cbc2cbcfc854c400944491d6cb
andie5 <andieasa@googlemail.com>
2016-12-08 21:15:51 +0000

    completed temperature tests

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -9,24 +9,70 @@
 # * options hashes
 
 class Temperature
-  def intialize(options = {})
+  def initialize(options = {})
     @options = options
   end
 
-  def in_farenheit
-    # if (@options.key?(:f))
-    #   @options[:f]
-    # else
-      ((@options[:f] * 9) / 5) + 32
-    # end
+
+# Change the hash value to fahrenheit or return the number of its already in fahrenheit
+# Use to_f to change a value to a float
+  def in_fahrenheit
+    if (@options.key?(:f))
+      @options[:f]
+    else
+      ((@options[:c].to_f * 9) / 5) + 32
+    end
   end
 
   def in_celsius
-    # if (@options.key?(:f))
-    #   @options[:f]
-    # else
-      ((@options[:c] - 32) * 5) / 9
-    # end
+    if (@options.key?(:c))
+      @options[:c]
+    else
+      ((@options[:f] - 32) * 5) / 9
+    end
   end
 
+  def self.from_fahrenheit(n)
+
+    # Could have used self.new.... here
+    Temperature.new(:f => n)
+  end
+
+  def self.from_celsius(n)
+    # Could have used self.new.... here
+    Temperature.new(:c => n)
+  end
+end
+
+# Extends the temperature class
+class Celsius < Temperature
+
+  def initialize(n, options = {})
+    @options = options
+     @options[:c] = n
+  end
+
+  def in_celsius
+    super
+  end
+
+  def in_fahrenheit
+    super
+  end
+end
+
+# Extends the temperature class
+  class Fahrenheit < Temperature
+  def initialize(n, options = {})
+    @options = options
+    @options[:f] = n
+  end
+
+  def in_celsius
+    super
+  end
+
+  def in_fahrenheit
+    super
+  end
 end
