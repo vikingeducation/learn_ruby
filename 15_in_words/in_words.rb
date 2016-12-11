d496415a00929517aad41eeb45b7fb0c8e9f89bd
andie5 <andieasa@googlemail.com>
2016-12-11 23:13:45 +0000

    completed tests for numbers in words

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -9,9 +9,9 @@
 # To make this test pass you'll need to extend a built-in class. You'll see that we're creating a new spec for 'Fixnum' -- this isn't a new class you'll be building, but instead it is a built-in class you will extend.
 # TDD
 
-class Fixnum
+# Words in numbers 
 
-  # res = ""
+class Fixnum
 
   def in_words
     if(self<10)
@@ -22,10 +22,14 @@ class Fixnum
       tens(self)
     elsif (self < 1000)
       hundreds(self)
-    elsif (self < 1000000)
+    elsif (self < 1_000_000)
       thousands(self)
     elsif (self < 1_000_000_000)
       millions(self)
+    elsif(self < 1_000_000_000_000)
+      billions(self)
+    elsif(self < 1_000_000_000_000_000)
+      trillions(self)
     end
 
   end
@@ -108,6 +112,7 @@ class Fixnum
 
     # puts "#{res} ten result"
   
+    # Then deal with the remainder
     if (remainder!=0)
       res.concat(" " + single_dig(remainder)) 
     end 
@@ -118,11 +123,21 @@ class Fixnum
    def hundreds(n)
     hundred = "hundred"
     remainder = n%100
+    divide_100 = n/100
+
+    # puts "#{divide_100} result of dividing by a hundred"
+    # puts "#{remainder} the remainder"
 
     # Get how many hundreds first
-    i = single_dig(n/100).concat(" " + hundred)
+    if (remainder == 0 || (divide_100 != 0 && divide_100 < 10))
+      i = single_dig(divide_100).concat(" " + hundred)
+    else 
+      i = ""
+    end
+    
+    # i = single_dig(n/100).concat(" " + hundred)
 
-    # Then utilise the tens and teens functions
+    # Then deal with the remainder
     if ((remainder != 0) && (remainder < 20))
       j = teens(remainder)
     else
@@ -135,47 +150,72 @@ class Fixnum
     thousand = "thousand"
     remainder = n%1000
     divide_1000 = n/1000
+
+    # puts "#{divide_1000} result of dividing by a thousand"
+    # puts "#{remainder} the remainder"
     
     # Get how many thousands first
-    if (remainder == 0)
+    if (remainder == 0 || divide_1000 < 10)
       i = single_dig(divide_1000).concat(" " + thousand)
     elsif((remainder !=0) && (divide_1000 < 20))
       i = teens(divide_1000).concat(" " + thousand)
     elsif((remainder !=0) && (divide_1000 < 100))
       i = tens(divide_1000).concat(" " + thousand)
+    elsif((remainder !=0) && (divide_1000 < 1000))
+      i = hundreds(divide_1000).concat(" " + thousand)
     end
 
-    # Then utilise the 100s function to do the rest
-    if(remainder != 0)
+    # puts "#{i} i in the thousands function"
+
+    # Then deal with the remainder
+    if((remainder != 0) && (divide_1000 < 10))
+      j = single_dig(remainder)
+    elsif((remainder != 0) && (divide_1000 < 20))
+      j = teens(remainder)
+    elsif(remainder != 0)
       j = hundreds(remainder)
     else
       j = ""
     end
 
+     # puts "#{j} j in the thousands function"
+
     return i.concat(" " + j).strip
   end
 
+
   def millions(n)
     million = "million"
-    remainder = n%1000000
-    divide_mill = n/1000000
+    remainder = n%1_000_000
+    divide_mill = n/1_000_000
 
-    remainder_1000 = n%1000
-    remainder_100 = n%1000
-    
+    # puts "#{divide_mill} result of dividing by a billion"
+    # puts "#{remainder} the remainder"
+
     # Get how many millions first
-    if (remainder == 0)
+    if (remainder == 0 || divide_mill < 10)
       i = single_dig(divide_mill).concat(" " + million)
-      puts "#{i} single"
+      # puts "#{i} single"
     elsif((remainder !=0) && (divide_mill < 20))
       i = teens(divide_mill).concat(" " + million)
-      puts "#{i} tens"
+      # puts "#{i} tens"
     elsif((remainder !=0) && (divide_mill < 100))
       i = tens(divide_mill).concat(" " + million)
+    elsif((remainder !=0) && (divide_mill < 1000))
+      i = hundreds(divide_mill).concat(" " + million)
+    elsif((remainder !=0) && (divide_mill < 1_000_000))
+      i = thousands(divide_mill).concat(" " + million)
     end
 
-    # Then utilise the 1000s function to do the rest
-    if(remainder != 0)
+
+    # puts "#{i} i in the millions function"
+
+    # Deal with the remainder
+    if((remainder != 0) && (remainder < 10))
+      j = single_dig(remainder)
+    elsif((remainder != 0) && (remainder < 20))
+      j = teens(remainder)
+    elsif(remainder != 0)
       j = thousands(remainder)
     else
       j = ""
@@ -184,5 +224,81 @@ class Fixnum
     return i.concat(" " + j).strip
   end
 
-  
+  def billions(n)
+    billion = "billion"
+    remainder = n%1_000_000_000
+    divide_bill = n/1_000_000_000
+
+    # puts "#{divide_bill} result of dividing by a billion"
+    # puts "#{remainder} the remainder"
+
+    # Get how many billions first
+    if (remainder == 0 || divide_bill < 10)
+      i = single_dig(divide_bill).concat(" " + billion)
+      # puts "#{i} single"
+    elsif((remainder !=0) && (divide_bill < 20))
+      i = teens(divide_bill).concat(" " + billion)
+      # puts "#{i} tens"
+    elsif((remainder !=0) && (divide_bill < 100))
+      i = tens(divide_bill).concat(" " + billion)
+    elsif((remainder !=0) && (divide_bill < 1000))
+      i = hundreds(divide_bill).concat(" " + billion)
+    end
+
+    # Deal with the remainder
+    if((remainder != 0) && (remainder < 10))
+      j = single_dig(remainder)
+      # puts "reached the single dig case"
+    elsif((remainder != 0) && (remainder < 20))
+      j = teens(remainder)
+      # puts "reached the teens case"
+    elsif(remainder != 0)
+      j = millions(remainder)
+      # puts "reached the thousands case"
+    else
+      j = ""
+    end
+
+    # puts "#{j} the result of j"
+
+    return i.concat(" " + j).strip
+  end
+
+  def trillions(n)
+    trillion = "trillion"
+    remainder = n%1_000_000_000_000
+    divide_trill = n/1_000_000_000_000
+
+    # puts "#{divide_trill} result of dividing by a trillion"
+    # puts "#{remainder} the remainder"
+
+    # Get how many trillions first
+    if (remainder == 0 || divide_trill < 10)
+      i = single_dig(divide_trill).concat(" " + trillion)
+      # puts "#{i} single"
+    elsif((remainder !=0) && (divide_trill < 20))
+      i = teens(divide_trill).concat(" " + trillion)
+      # puts "#{i} tens"
+    elsif((remainder !=0) && (divide_trill < 100))
+      i = tens(divide_trill).concat(" " + trillion)
+    end
+
+    # Deal with the remainder
+    if((remainder != 0) && (remainder < 10))
+      j = single_dig(remainder)
+      # puts "reached the single dig case"
+    elsif((remainder != 0) && (remainder < 20))
+      j = teens(remainder)
+      # puts "reached the teens case"
+    elsif(remainder != 0)
+      j = billions(remainder)
+      # puts "reached the thousands case"
+    else
+      j = ""
+    end
+
+    # puts "#{j} the result of j"
+
+    return i.concat(" " + j).strip
+  end 
 end
