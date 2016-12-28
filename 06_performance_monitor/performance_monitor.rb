8d0c74822d6dc68c3808c7d318ad0d2ec142e8bd
Stephen Akuffo <slyven@gmail.com>
2016-12-28 21:48:11 +0000

    performance_monitor complete

--- 06_performance_monitor/performance_monitor.rb
+++ 06_performance_monitor/performance_monitor.rb
@@ -6,5 +6,5 @@ def measure(num_times = 1)
     avg_time << (Time.now - beginning_time)
   end
 
-  return avg_time.inject{ |sum, el| sum + el }.to_f / avg_time.size
+  return avg_time.inject{ |total_time, bench_time| total_time + bench_time }.to_f / avg_time.size
 end
