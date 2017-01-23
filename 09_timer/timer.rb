5bc9124357830df26c4742e2f4d2846254e66282
Roy Chen <roychenlei@gmail.com>
2017-01-23 21:25:35 +0800

    completed time_string and padded methods

--- 09_timer/timer.rb
+++ 09_timer/timer.rb
@@ -6,5 +6,18 @@ class Timer
   end
 
   def time_string
+    hours = @seconds / 60 / 60
+    minutes = @seconds / 60 % 60
+    seconds = @seconds % 60
+
+    "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"
+  end
+
+  def padded(number)
+    if number.to_s.length == 2
+      number.to_s
+    else
+      "0#{number}"
+    end
   end
 end
