10b80ccae09f5ceb15925ec8b5ba0f7072212e40
Roy Chen <roychenlei@gmail.com>
2017-01-23 14:30:25 +0800

    completed reverser method

---
+++ 05_silly_blocks/silly_blocks.rb
@@ -0,0 +1,10 @@
+def reverser
+  block = yield
+  if block.split(" ").length > 1
+    strings = block.split(" ")
+    strings.map { |string| string.reverse! }
+    return strings.join(" ")
+  else
+    return block.reverse
+  end
+end
