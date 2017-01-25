d0477d47825a83cb3a8ff6f1257940c781b8b95c
Roy Chen <roychenlei@gmail.com>
2017-01-25 12:40:32 +0800

    completed hello method

---
+++ 13_xml_document/xml_document.rb
@@ -0,0 +1,21 @@
+class XmlDocument
+  def initialize
+  end
+
+  def hello(options = {})
+    if options.empty?
+      return "<hello/>"
+    end
+
+    start_tag = "<hello"
+    end_tag = "/>"
+    attributes = ""
+    
+    options.each do |key, value|
+      attribute = "#{key}='#{value}'"
+      attributes += "#{attribute} "
+    end
+
+    "#{start_tag} #{attributes.rstrip!}#{end_tag}"
+  end
+end
